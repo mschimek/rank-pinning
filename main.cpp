3f49b2533a4212aa1c329d83516f3322a9b75e61
Matthias Schimek <schimek@kit.edu>
2025-09-17 02:45:52 +0200

    print hex

--- main.cpp
+++ main.cpp
@@ -71,7 +71,7 @@ void print_pinning() {
                   << "/" << std::setw(5) << info_object.num_threads
                   << " on logical core: " << std::setw(5)
                   << info_object.logical_core_id << std::setw(9)
-                  << " cpu mask: " << info_object.cpu_mask << " node:" << node
+                  << " cpu mask: " << std::hex << info_object.cpu_mask << " node:" << node
                   << std::endl;
       }
     }
